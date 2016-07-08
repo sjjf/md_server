0a94bb2df5469216799a8a68d60c05fde49ade36
Chandan Dutta Chowdhury <chandanc@juniper.net>
2016-07-08 10:53:22 +0530

    Fix: packaging

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from distutils.core import setup
 
 setup(name='mdserver',
       description='Simple metadata server',
+      version='0.1',
+      url='https://bitbucket.org/xchandan/md_server',
       author='Chandan Dutta Chowdhury',
       author_email='chandan.dutta.chowdhury@gmail.com',
       packages=['mdserver'],
-      package_dir={'etc': 'etc'},
-      package_data={'etc': ['etc/mdserver/*']})
+      data_files=[('/etc/mdserver', ['etc/mdserver/mdserver.conf']),
+                  ('/usr/bin', ['bin/mdserver']),
+                  ('/etc/systemd/system/mdserver.service',
+                   ['etc/systemd/system/mdserver.service'])
+                  ])
