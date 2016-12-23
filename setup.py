3e5c0dcd17540dfce4750645a4d1c21c8466370b
Simon Fowler <simon.fowler@anu.edu.au>
2016-12-23 12:20:31 +1100

    Install everything . . .
    Apparently I /do/ need to install init scripts and the like, since this
    isn't being packaged. So the inelegant solution is to simply install
    both sysv and systemd configs.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 
+
 setup(name='mdserver',
       description='Simple metadata server',
       version='0.1',
@@ -8,4 +9,10 @@ setup(name='mdserver',
       author_email='chandan.dutta.chowdhury@gmail.com',
       packages=['mdserver'],
       install_requires=[line.rstrip() for line in open('requirements.txt')],
-      scripts=['bin/mdserver'])
+      scripts=['bin/mdserver'],
+      data_files=[
+          ('/etc/mdserver', ['etc/mdserver/mdserver.conf']),
+          ('/etc/default', ['etc/default/mdserver']),
+          ('/etc/init.d', ['etc/init/sysv/mdserver'])
+          ('/etc/systemd/system', ['etc/init/systemd/mdserver.service'])
+          ])
